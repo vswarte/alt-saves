1aa915694c25a79dc936993bf3efeb33ab7bfffc
Vincent Swarte <vincentswarte@gmail.com>
2023-06-27 20:55:23 +0200

    Experimental ER-only regbin checksum neuter

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,9 @@ mod config;
 
 use broadsword::dll;
 use broadsword::runtime;
+use broadsword::runtime::get_module_handle;
 use detour::static_detour;
+use log::info;
 
 use windows::core::{HSTRING, PCWSTR};
 use windows::Win32::Foundation::HANDLE;
@@ -17,6 +19,8 @@ const SAVEGAME_BACKUP_EXTENSION: &str = ".sl2.bak";
 const SC_SAVEGAME_EXTENSION: &str = ".co2";
 const SC_SAVEGAME_BACKUP_EXTENSION: &str = ".co2.bak";
 
+const REGBIN_CHECK_FLAG_IBO: usize = 0x3acea92;
+
 static_detour! {
     static CREATE_FILE_W_HOOK: unsafe extern "system" fn(PCWSTR, u32, u32, u64, u32, u32, HANDLE) -> u64;
 }
@@ -26,6 +30,16 @@ pub fn entry(_: usize) -> bool {
     broadsword::logging::init("log/alt-saves.log");
     apply_hooks();
     return true;
+}
+
+// Overwrites the flag that seems to determine if the regulation bin file should be checked against
+// a particular hash. This check causes new save files to throw errors when the regbin has been
+// changed.
+fn apply_regbin_check_patch() {
+    let ptr = get_module_handle("eldenring.exe".to_string())
+        .expect("Could not find ER base") + REGBIN_CHECK_FLAG_IBO;
+
+    unsafe { *(ptr as *mut u8) = 0x0 };
 }
 
 fn apply_hooks() {
@@ -43,6 +57,8 @@ fn apply_hooks() {
                       creation_disposition: u32,
                       flags_and_attributes: u32,
                       template_file: HANDLE| {
+
+                    apply_regbin_check_patch();
 
                     // Doing this here to ensure the string isn't dropped until after the fn call
                     // otherwise the string's source is dropped before the pointer is consumed.
